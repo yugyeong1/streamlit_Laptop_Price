4e3a1875146eb7a6f66e4f2b64194995172f5340
yugyeong1 <dbrud1032@gmail.com>
2022-12-20 18:05:27 +0900

    user_choice 수정

--- app_user_choice_product.py
+++ app_user_choice_product.py
@@ -10,6 +10,21 @@ laptop['Ram'] = laptop['Ram'].str.split('G').str[0].astype(float)
 laptop = laptop.drop(columns= 'laptop_ID')
 
 def run_app_user_choice():
+
     st.text('')
     st.text('')
     st.markdown('#### 유저가 선택한 데이터의 값을 나타냅니다.')
+
+    col1, col2 = st.columns(2)
+
+    with col1:
+        pass
+
+    with col2:
+        company_choice = st.selectbox(' ', laptop['Company'].unique())
+        type_choice = st.selectbox(' ', laptop['TypeName'].unique())
+    
+
+    st.dataframe(laptop[laptop['Company']== company_choice])
+
+
